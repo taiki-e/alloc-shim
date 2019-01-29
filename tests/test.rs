4a3ea0575732ba034b29ad989a81b6a3674f9dfd
Taiki Endo <te316e89@gmail.com>
2019-01-29 20:44:47 +0900

    Rewrite to the same layout as the alloc crate

--- tests/test.rs
+++ tests/test.rs
@@ -1,19 +1,18 @@
 #![deny(warnings)]
 #![allow(unused_imports)]
-#![cfg_attr(
-    feature = "alloc",
-    feature(futures_api, core_panic_info, core_intrinsics, raw)
-)]
-
-use alloc::{
-    alloc as alloc_mod, any, arch, ascii, borrow, cell, char, clone, cmp, convert, default, f32,
-    f64, ffi, fmt, hash, hint, i128, i16, i32, i64, i8, isize, iter, marker, mem, num, ops, option,
-    prelude, prelude::v1 as core_v1, ptr, result, slice, str, sync, sync::atomic as core_atomic,
-    time, u128, u16, u32, u64, u8, usize,
-};
+#![cfg_attr(all(feature = "alloc", not(feature = "std")), feature(alloc))]
+#![cfg_attr(feature = "futures", feature(futures_api))]
 
 #[cfg(any(feature = "alloc", feature = "std"))]
-use alloc::{boxed, collections, panic, prelude::v1, rc, string, sync::atomic, sync::Arc, vec};
+mod test {
+    use alloc::{
+        alloc as alloc_mod, borrow, boxed, collections, fmt, prelude, prelude::Box, prelude::Vec,
+        rc, slice, str, string, sync, sync::Arc, vec,
+    };
 
-#[cfg(feature = "alloc")]
-use alloc::{future, intrinsics, pin, raw, task};
+    // `alloc::sync` does not include `atomic` module
+    // use alloc::sync::atomic;
+
+    #[cfg(unstable)]
+    use alloc::task;
+}
